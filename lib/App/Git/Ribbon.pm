eac748c6615d54628998aa622ee518fff4119dfa
Eric D Johnson <cpan.nospamthanks@iijo.org>
2013-02-10 19:48:42 +0000

    Import of KABLAMO/App-Git-Ribbon-0.001 from CPAN.
    gitpan-cpan-distribution: App-Git-Ribbon
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         KABLAMO/App-Git-Ribbon-0.001.tar.gz
    gitpan-cpan-author:       KABLAMO
    gitpan-cpan-maturity:     released

---
+++ lib/App/Git/Ribbon.pm
@@ -0,0 +1,37 @@
+package App::Git::Ribbon;
+
+our $VERSION = '0.001'; # VERSION
+
+# ABSTRACT: Review the latest changes to a git repository
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Git::Ribbon - Review the latest changes to a git repository
+
+=head1 SYNOPSIS
+
+  git ribbon
+
+=head2 DESCRIPTION
+
+TODO
+
+=head1 AUTHOR
+
+Eric Johnson <cpan at iijo dot nospamthanks dot org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2013 by Eric Johnson.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
+
+=cut
